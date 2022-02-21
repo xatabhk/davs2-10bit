6c644428b4ee8999bb146ae81b8f36abd4213411
zheng tianbo <naturalwalker@hotmail.com>
2022-02-21 17:19:15 +0800

    Add alf_filter_block_sse128_10bit and alf_filter_block_avx_10bit

--- source/common/alf.cc
+++ source/common/alf.cc
@@ -515,6 +515,13 @@ void davs2_alf_init(uint32_t cpuid, ao_funcs_t *fh)
     /* init asm function handles */
 #if HAVE_MMX
 #if HIGH_BIT_DEPTH
+        if (cpuid & DAVS2_CPU_SSE4) {
+            fh->alf_block[0] = alf_filter_block_sse128_10bit;
+        }
+        if (cpuid & DAVS2_CPU_AVX) {
+            fh->alf_block[0] = alf_filter_block_avx_10bit;
+        }
+
 #else
     if (cpuid & DAVS2_CPU_SSE4) {
         fh->alf_block[0] = alf_filter_block_sse128;
